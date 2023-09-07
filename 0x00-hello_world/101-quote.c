743d0dbd3ab227a46b62376bf287ad479bdaae58
dina <dina.hamadi.20@gmail.com>
2023-09-07 12:40:29 -0700

    hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, &str, strlen(str));
+	return (0);
+}
