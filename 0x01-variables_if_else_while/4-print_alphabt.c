80917e6a456203fff40508bb8d8409268442ffe1
dina <dina.hamadi.20@gmail.com>
2023-09-08 12:17:02 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet except for e and q
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		if (i != 101 && i != 113)
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
