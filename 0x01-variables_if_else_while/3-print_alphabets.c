7903729698b1c48afcb710790b89a6a651e3d919
dina <dina.hamadi.20@gmail.com>
2023-09-08 12:11:06 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (i = 65; i < 90; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
