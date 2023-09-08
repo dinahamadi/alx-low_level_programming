df20206114d81b3401c60138f7d67d5328df52b2
dina <dina.hamadi.20@gmail.com>
2023-09-08 12:03:01 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
