d7dd4f50b63269e331e1b07b74344f74805fb0d6
dina <dina.hamadi.20@gmail.com>
2023-09-08 12:49:21 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints combinations of double digits numbers
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
