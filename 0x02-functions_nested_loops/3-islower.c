41ce003c2a55d0d90d0fea817b1495ca7fed1bc7
dina <dina.hamadi.20@gmail.com>
2023-09-12 16:43:30 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ * @c: a character
+ * Return: 1 if c is lowercase 0 otherwise.
+ */
+int _islower(int c)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if ( c == i)
+			return (1);
+
+	}
+	return (0);
+}
