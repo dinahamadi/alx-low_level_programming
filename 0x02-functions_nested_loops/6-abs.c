df50ae9385328a1d2e143be40be7ae05ceb87761
dina <dina.hamadi.20@gmail.com>
2023-09-12 17:06:34 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - check the code
+ * @i: an integer
+ * Return: return the absolute value of an integer.
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
