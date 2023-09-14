b230833469398ddad3ecaf11eb355a5fc6311ab2
dina <dina.hamadi.20@gmail.com>
2023-09-14 16:46:39 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ * 
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
