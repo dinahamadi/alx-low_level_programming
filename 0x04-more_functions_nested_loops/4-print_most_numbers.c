62f981f7ab615e15b6ef337385e125e63aacd467
dina <dina.hamadi.20@gmail.com>
2023-09-14 16:50:09 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * print_numbers - check the code
- *
+ * print_most_numbers - check the code 
  * Return: Nothing
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
+		if (i != '2' && i != '4')
 		_putchar(i);
 	}
 	_putchar('\n');
