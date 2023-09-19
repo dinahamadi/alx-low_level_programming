8e4971303ba692d2d232d349b8d452d4c858087f
dina <dina.hamadi.20@gmail.com>
2023-09-18 19:59:25 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 
 /**
  * print_rev - check the code
- *
+ * @s: a string
  * Return: no return.
  */
 void print_rev(char *s)
 {
 	int j;
 
-	for (j = strlen(s); j > 0; j--)
+	for (j = strlen(s); j = 0; j--)
 		_putchar(s[j]);
 	_putchar('\n');
 }
