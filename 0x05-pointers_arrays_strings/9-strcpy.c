98727a3e73636fbeea96b2bf09492a871a2e73a3
dina <dina.hamadi.20@gmail.com>
2023-09-19 11:14:27 -0700

    pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * _strcpy - check the code
  * @dest: destination string
@@ -11,8 +11,15 @@ char *_strcpy(char *dest, char *src)
 	size_t i = 0;
 
 	if (strlen(src) > strlen(dest))
-		dest = realloc(sizeof(char) * (strlen(src) + 1));
-	while (*src != '\0')
+	{
+		dest = malloc(sizeof(char) * (strlen(src) + 1));
+		if (dest == NULL)
+		{
+			free(dest);
+			return (NULL);
+		}
+	}
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
