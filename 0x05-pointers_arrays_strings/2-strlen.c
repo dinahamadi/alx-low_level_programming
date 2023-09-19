855d62c3e9fe2bb26d8e1aef4d7f3d7ed9d8bfb8
dina <dina.hamadi.20@gmail.com>
2023-09-18 19:39:47 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ * @s: a string
+ * Return: the string lenght.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while(*str != '\0')
+		count++;
+	return (count);
+}
