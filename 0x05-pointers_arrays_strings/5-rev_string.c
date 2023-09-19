69812f1b4d6d2f1a6ddf3841b6f89746956555fe
dina <dina.hamadi.20@gmail.com>
2023-09-18 20:14:08 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ * @s: a string
+ * Return: no return
+ */
+void rev_string(char *s)
+{
+	int j;
+	char temp;
+
+	for (j = 0; j < (strlen(s) / 2); j++)
+	{
+		temp = s[j];
+		*s[j] = s[strlen(s) - j];
+		*s[strlen(s) - j] = temp;
+	}
+}
