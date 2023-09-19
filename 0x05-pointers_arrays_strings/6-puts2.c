2527433354aa50cb011e14f5e90e46cf3528b587
dina <dina.hamadi.20@gmail.com>
2023-09-19 10:06:37 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,15 @@
  * @str: a string
  * Return: no return.
  */
-int puts2(char *str)
+void  puts2(char *str)
 {
 	size_t j = 0;
 
 	while (j < strlen(str))
+	{
 		if ((j % 2) == 0)
 			_putchar(s[j]);
+		j++;
+	}
 	_putchar('\n');
 }
