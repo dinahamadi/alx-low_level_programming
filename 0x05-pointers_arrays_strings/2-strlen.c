3542fb6965468b77797f20a3a0cd55030bda92b0
dina <dina.hamadi.20@gmail.com>
2023-09-18 19:45:06 -0700

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,6 +10,9 @@ int _strlen(char *s)
 	int count = 0;
 
 	while(*s != '\0')
+	{
 		count++;
+		s++;
+	}
 	return (count);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: a string
+ * Return: a string.
+ */
+void _puts(char *str)
+{
+return (str);
+}
