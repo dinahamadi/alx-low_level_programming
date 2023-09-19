0f5e66bf2181c3e5701f39c7429382f278e011e5
dina <dina.hamadi.20@gmail.com>
2023-09-18 19:31:30 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code 
+ *
+ * Return: No return.
+ */
+void reset_to_98(int *n)
+{
+n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * Return: no return.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = a;
+	a = b;
+	b = c;
+}
