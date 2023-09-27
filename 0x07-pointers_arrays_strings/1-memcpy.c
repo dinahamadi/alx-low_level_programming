2eaf0e190860900386c4b5960f2d2f83a5e734f3
dina <dina.hamadi.20@gmail.com>
2023-09-26 20:37:17 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -21,7 +21,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 			return (NULL);
 		}
 	}
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
 	return (dest);
