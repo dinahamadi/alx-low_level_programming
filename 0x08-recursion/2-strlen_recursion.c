c36c2f4a172a0d4b9ba3f56661ced949b94fac51
dina <dinahamadi.20@gmail.com>
2023-09-30 07:36:58 +0100

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-*_strlen_recursion: chack the code
+*_strlen_recursion: check the code
 * @s: astring
 * Return: an integer
 */
 
 int _strlen_recursion(char* s)
 {
-	if (s != NULL)
-		return (1 + _strlen_recursion(s + 1));
+	if (s == NULL || *s == '\0')
+		return (0); 
 	else
-			return (0);
-}
+		return (1 + _strlen_recursion(s + 1));
+	}
