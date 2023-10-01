fca652aaff0b844696a20fcaeb0a4a56056bdabd
dina <dinahamadi.20@gmail.com>
2023-10-02 00:30:46 +0100

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,11 +8,8 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	if (n == 0)
+		return (0);
 	else
-	{
-		if (n == 0)
-			return(0);
-		else
-			return (n + factorial(n - 1));
-	}
- }
+		return (n * factorial(n - 1));
+}
