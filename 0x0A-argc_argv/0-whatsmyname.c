2eba0ef56453e3eca1a8a46c174602efdb866588
dina <dinahamadi.20@gmail.com>
2023-10-02 21:50:22 +0100

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: number of arguments
+* @argv: arguments
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s", argv[0]);
+	return (0);
+}
