1d479d2853889a7449760b5fb331f71a0036fa0a
dina <dinahamadi.20@gmail.com>
2023-10-02 02:42:26 +0100

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,12 +17,12 @@ int is_prime_helper(int p, int n)
 
 /**
  * is_prime_number - check the code
- * @n; an integer
+ * @n: an integer
  * Return: 1 if true 0 otherwise.
  */
 int is_prime_number(int n)
 {
-	if (n < 1)
-		return (-1);
+	if (n < 0)
+		return (is_prime_number(-1 * n));
 	return (is_prime_helper(n - 1, n));
 }
