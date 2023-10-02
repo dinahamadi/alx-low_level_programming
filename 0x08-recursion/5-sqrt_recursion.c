ab7cfee8fe0bf98043d979bf5706c0c9bcd1ef73
dina <dinahamadi.20@gmail.com>
2023-10-02 01:38:43 +0100

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,15 +7,18 @@
  * @sq: an integer
  * Return: the natural square root.
  */
-int _sqrt_helper(int sq, int n)
+int _sqrt_helper(int sqb,int  sqh, int n)
 {
-	if (sq < 1)
+	int mid;
+
+	if (sqh - sqb < 0)
 		return (-1);
-	if (sq * sq == n)
-		return (sq);
-	if (sq * sq > n)
-		return (_sqrt_helper(sq - 1, n));
-	return (_sqrt_helper(sq + 1, n));
+	mid = (sqh + sqb) / 2;
+	if (mid * mid == n)
+		return (mid);
+	if (mid * mid > n)
+		return (_sqrt_helper(sqb, (sqh + mid) / 2,n));
+	return (_sqrt_helper((sqb + mid) / 2, sqh, n));
 }
 /**
  * _sqrt_recursion - check the code
@@ -28,5 +31,5 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 1)
 		return (1);
-	return (_sqrt_helper(n / 2, n));
+	return (_sqrt_helper(0, n / 2, n));
 }
