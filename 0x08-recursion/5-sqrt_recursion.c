07053d0d05257cec8a9a80212a12c2371002d2dd
dina <dinahamadi.20@gmail.com>
2023-10-02 02:06:01 +0100

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,13 +10,13 @@
  */
 int _sqrt_helper(int sq, int n)
 {
-	if (sq == 0)
+	if (sq == 0 || sq == 1)
 		return (-1);
 	if (sq * sq == n)
 		return (sq);
 	if (sq * sq < n)
 		return (_sqrt_helper(sq + sq / 2, n));
-	return (_sqrt_helper(sq + sq / 2, n));
+	return (_sqrt_helper(sq - sq / 2, n));
 }
 /**
  * _sqrt_recursion - check the code
