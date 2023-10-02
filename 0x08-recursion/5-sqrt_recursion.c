0076fc4b06731831dcdf5263b815e761653a4b60
dina <dinahamadi.20@gmail.com>
2023-10-02 01:02:00 +0100

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_helper - check the code
+ * @n: an integer
+ * Return: the natural square root.
+ */
+int _sqrt_helper(int q, int n)
+{
+	if (q < 1)
+		return (-1);
+	else
+	{
+		if (sq * sq == n)
+			return (sq);
+		else
+		{
+			return (_sqrt_recursion(sq - 1, n))
+		}
+	}
+}
+/**
+ * _sqrt_recursion - check the code
+ * @n: an integer
+ * Return: the natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 1)
+		return (-1);
+	return (_sqrt_helper(n / 2, n));
+}
