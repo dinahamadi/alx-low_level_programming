029e9ebc74e35d589b0403551bf789ea351fffee
dina <dinahamadi.20@gmail.com>
2023-10-02 01:14:54 +0100

    reccrsion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,19 +4,17 @@
 /**
  * _sqrt_helper - check the code
  * @n: an integer
+ * @sq: an integer
  * Return: the natural square root.
  */
 int _sqrt_helper(int sq, int n)
 {
 	if (sq < 1)
 		return (-1);
+	if (sq * sq == n)
+		return (sq);
 	else
-	{
-		if (sq * sq == n)
-			return (sq);
-		else
-			return (_sqrt_helper(sq - 1, n));
-	}
+		return (_sqrt_helper(sq - 1, n));
 }
 /**
  * _sqrt_recursion - check the code
