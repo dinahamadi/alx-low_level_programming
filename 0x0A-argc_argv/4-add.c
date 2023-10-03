4d5a2bbb52feac4fd161184be029e17dc61766a6
dina <dinahamadi.20@gmail.com>
2023-10-03 02:06:53 +0100

    argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - Entry point
+* @argc: number of arguments
+* @argv: arguments
+* Return: Always 0 (Success)
+*/
+int main(int argc, char* argv[])
+{
+	int count = 0, i = 1;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		int j = 0;
+
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				j++;
+		}
+		count += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", count);
+	return (0);
+}
