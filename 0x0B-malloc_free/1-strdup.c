8ec8204046842a52406453afe462dad48bc776d2
dina <dinahamadi.20@gmail.com>
2023-10-05 01:43:17 +0100

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strdup - check the code.
+ * @str: a string
+ * Return: a pointer to the new string.
+ */
+char *_strdup(char* str)
+{
+	char* s2;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	s2 = malloc(sizeof(char) * (strlen(str) + 1));
+	if (s2 == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		s2[i] = str[i];
+		i++;
+	}
+	return (s2);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* create_array(unsigned int size, char c);
 char* _strdup(char* str);
