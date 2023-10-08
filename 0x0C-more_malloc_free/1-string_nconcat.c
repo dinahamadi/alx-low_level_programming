bc441684584074c1d0d36f3ffe22db826e8c433d
dina <dinahamadi.20@gmail.com>
2023-10-08 21:54:26 +0100

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (n > strlen(s2))
+		n = strlen(s2);
 	s3 = malloc(sizeof(char) * (strlen(s1) + n + 1));
 	if (s3 == NULL)
 		return (NULL);
