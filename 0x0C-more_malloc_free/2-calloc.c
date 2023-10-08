adc1374b0371ac13466722e1a685644d57bc9cad
dina <dinahamadi.20@gmail.com>
2023-10-08 22:25:54 +0100

    more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(size * nmemb);
+	p = malloc(size * nmemb + 1);
 	if (p == NULL)
 		return (NULL);
 	while (i < nmemb)
