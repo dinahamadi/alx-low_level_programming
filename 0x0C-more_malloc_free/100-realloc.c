6cb7f76313d259c6fda9909e1ca602995a2c7be5
dina <dinahamadi.20@gmail.com>
2023-10-09 00:26:39 +0100

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,15 +9,14 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *nptr;
+	char *nptr;
 	unsigned int i, min;
 
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0)
 	{
-		if (ptr != NULL)
-			free(ptr);
+		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL && new_size != 0)
