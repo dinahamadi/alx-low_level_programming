7a408fb1d587ba97ffd431eec17f6e8f614e03ce
dina <dinahamadi.20@gmail.com>
2023-10-09 02:28:47 +0100

    malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - check code
+ * @**grid: a pointer to a two dim grid
+ * @height: height of the grid
+ * Return: no return.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
