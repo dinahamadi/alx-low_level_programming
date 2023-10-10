36f53efac8ce841b5a2a53a6a59075f00593bf45
dina <dinahamadi.20@gmail.com>
2023-10-10 23:36:23 +0100

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+/**
+ * struct dog - a new type that has dogs informations
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
