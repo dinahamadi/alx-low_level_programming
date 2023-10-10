9a42295f303c4295351a792a904dca9906910a70
dina <dinahamadi.20@gmail.com>
2023-10-11 00:23:37 +0100

    struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", strcmp(d->name, "") == 0 ? "(nil)" : d->name);
+	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
 	if (d->age < 0)
 		printf("Age: %s\n", "(nil)");
 	else
 		printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", strcmp(d->owner, "") == 0 ? "(nil)" : d->owner);
+	printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
