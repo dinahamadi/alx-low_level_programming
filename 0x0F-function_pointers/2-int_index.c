468b2cec9659e3a71ede909ff5e3babf1875ca28
dina <dinahamadi.20@gmail.com>
2023-10-11 22:52:43 +0100

    p_func

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - check code
+ * @array: array of integes
+ * @size: an integer
+ * @cmp: a function pointer
+ * Return: the index of the first element that cmp does not return 0
+ * or -1 otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	while (i <= size)
+	{
+		if (cmp(array[i]) == 1)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
