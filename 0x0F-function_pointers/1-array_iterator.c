9733d9e5b179e67dabf8057761038644d76ddd7c
dina <dinahamadi.20@gmail.com>
2023-10-11 22:05:21 +0100

    p_func

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - check code
+ * @array: an integer
+ * @size: unsigned integer
+ * @action: a function pointer
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i = 0;
+if (action)
+for (i = 0; i < size; i++)
+{
+	action(array[i]);
+}
+}
