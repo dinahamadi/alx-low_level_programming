3be52444c5ac4b03046bdd015289123ff215336d
dina <dinahamadi.20@gmail.com>
2023-10-11 20:01:26 +0100

    p_func

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - check the code
+ * @name: name of the person
+ * @f: a pointer to function
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,17 @@
+#ifndef functions_h
+#define functions_h
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+
+
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+
+#endif
