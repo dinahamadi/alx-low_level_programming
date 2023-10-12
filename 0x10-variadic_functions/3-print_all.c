1774f8279ef3ff59e66e6638a21ddbfae030d577
dina <dinahamadi.20@gmail.com>
2023-10-12 22:58:16 +0100

    variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,17 +1,16 @@
-#include "variadic_functions.h"
-
+#include "variadic_functions.h
 /**
- * print_all - check the code
- * @format: a string
- * Return: No return.
- */
+* print_all - check the code
+* @format: a string
+* Return: No return.
+*/
 void print_all(const char *const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
 	char *str;
 	int valid = 1;
-	
+
 	if (format == NULL)
 		return;
 	va_start(args, format);
@@ -19,27 +18,27 @@ void print_all(const char *const format, ...)
 	{
 		switch (format[i])
 		{
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			str = va_arg(args, char*);
-			if (str == NULL)
-			{
-				printf("(nil)");
+			case 'i':
+				printf("%d", va_arg(args, int));
 				break;
-			}
-			printf("%s", str);
-			break;
-		default:
-			valid = 0;
-			break;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char*);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				valid = 0;
+				break;
 		}
 		if ((i < strlen(format) - 1) && (valid == 1))
 			printf(", ");
