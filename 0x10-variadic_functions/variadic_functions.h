fde4c0a71426cb7c33b7bfc5f98c754fd8e61285
dina <dinahamadi.20@gmail.com>
2023-10-12 19:35:39 +0100

    variadic

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef variadic_h
+#define variadic_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char *const format, ...);
+
+
+
+
+#endif
