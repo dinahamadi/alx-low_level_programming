efe5cdfafb77415887a193a65e2acd70b7f76b62
dina <dinahamadi.20@gmail.com>
2023-10-12 15:44:13 +0100

    advanced

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,10 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	FILE *fil;
-	size_t let_r;
 	int i;
-	char *buffer;
+	unsigned char *opc = (unsigned char *)main;
 
 	if (argc != 2)
 	{
@@ -24,27 +22,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (2);
 	}
-	fil = fopen(__FILE__, "rb");
-	if (fil == NULL)
+	for (i = 0; i < atoi(argv[1]); i++)
 	{
-		fclose(fil);
-		return (1);
-	}
-	buffer = malloc(sizeof(char) * atoi(argv[1]));
-	if (buffer == NULL)
-		return (1);
-	let_r = fread(buffer, sizeof(char), atoi(argv[1]), fil);
-	if (let_r == atoi(argv[1]))
-	{
-		for (i = 0; i < atoi(argv[1]); i++)
-		{
-			printf("%2hhx", buffer[i]);
-			if (i != (atoi(argv[1]) - 1))
-				printf(" ");
-		}
+		printf("%02x", opc[i]);
+		if (i != (atoi(argv[1]) - 1))
+			printf(" ");
 	}
 	printf("\n");
-	fclose(fil);
-	free(buffer);
 	return (0);
 }
