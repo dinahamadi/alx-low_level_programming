681e84621e9837abea343bdcbb657e3371a448ab
dina <dinahamadi.20@gmail.com>
2023-10-19 22:25:11 +0100

    lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - check the code
+ * @h: a list
+ * Return: number od elements in the lint.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h->next)
+	{
+		count++;
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+	}
+	return (count);
+}
