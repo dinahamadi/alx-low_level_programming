d342429c9a66299b1a4c01092cbf68742b7dada7
dina <dinahamadi.20@gmail.com>
2023-12-16 00:17:39 +0100

    bit manip

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,6 +10,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	int result = 0;
 
+	if (index > sizeof(n) * 8)
+		return (-1);
 	result = (*n >> index) & 1;
 	if (result == 1)
 		return (1);
