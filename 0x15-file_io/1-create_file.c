6d619c40dd31a79966928fd6e555adafa98837d4
dina <dinahamadi.20@gmail.com>
2023-12-19 01:34:06 +0100

    iofiles

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL || *filename == '\0')
 		return (-1);
-	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL || *text_content == '\0')
