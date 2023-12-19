2c53adb2cab4a64b3e1604b802a07ed6aaa03c6b
dina <dinahamadi.20@gmail.com>
2023-12-19 03:43:38 +0100

    iofiles

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,38 +24,24 @@ void fdclose(int fd)
 int main(int argc, char *argv[])
 {
 	ssize_t fd1, fd2, nwrite = 0, nread = 0;
-	char buff[1024], *file_from, *file_to;
+	char buff[1024];
 
 	argc != 3 ? perror(USAGE), exit(EXIT_USAGE) : (void)NULL;
-	file_from = strdup(argv[1]);
-	if (!file_from)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(EXIT_READ);
-	}
-	fd1 = open(file_from, O_RDONLY);
-	fd1 == -1 ? free(file_from), dprintf(STDERR_FILENO,
-		"Error: Can't read from file %s\n", file_from), exit(EXIT_READ) : (void)
-		NULL;
-	file_to = strdup(argv[2]);
-	file_to ? (void)NULL : free(file_from), fdclose(fd1), dprintf(STDERR_FILENO,
-		"Error: Can't write to %s\n", file_to), exit(EXIT_WRITE);
-	fd2 = open(file_to, O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	fd2 == -1 ? free(file_from), fdclose(fd1), dprintf(STDERR_FILENO,
-		"Error: Can't write to %s\n", file_to), free(file_to), exit(EXIT_WRITE) :
-		(void)NULL;
+	fd1 = open(argv[1], O_RDONLY);
+	fd1 == -1 ? dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		argv[1]), exit(EXIT_READ) : (void)NULL;
+	fd2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	fd2 == -1 ? fdclose(fd1), dprintf(STDERR_FILENO, "Error: Can't write to %s\n"
+		, argv[2]), exit(EXIT_WRITE) : (void)NULL;
 	while ((nread = read(fd1, buff, 1024)) > 0)
 	{
 		nwrite = write(fd2, buff, nread);
-		nwrite == -1 ? free(file_from), fdclose(fd1), dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file_to), free(file_to), exit(EXIT_WRITE) :
-			(void)NULL;
+		nwrite == -1 ? fdclose(fd1), dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[1]), exit(EXIT_WRITE) : (void)NULL;
 	}
-	nread == -1 ? free(file_from), fdclose(fd1), dprintf(STDERR_FILENO,
-		"Error: Can't read from file %s\n", file_from), exit(EXIT_READ) : (void)NULL;
-	free(file_from);
+	nread == -1 ? fdclose(fd1), dprintf(STDERR_FILENO,
+		"Error: Can't read from file %s\n", argv[1]), exit(EXIT_READ) : (void)NULL;
 	fdclose(fd1);
 	fdclose(fd2);
-	free(file_to);
 	return (0);
 }
