7535a9d023730d70b20568792ec1734f1b7ff44b
dina <dinahamadi.20@gmail.com>
2023-12-19 03:54:26 +0100

    iofiles

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,7 +30,8 @@ int main(int argc, char *argv[])
 	fd1 = open(argv[1], O_RDONLY);
 	fd1 == -1 ? dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 		argv[1]), exit(EXIT_READ) : (void)NULL;
-	fd2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY | O_APPEND, 0664);
+	fd2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR
+		| S_IRGRP | S_IWGRP | S_IROTH);
 	fd2 == -1 ? fdclose(fd1), dprintf(STDERR_FILENO, "Error: Can't write to %s\n"
 		, argv[2]), exit(EXIT_WRITE) : (void)NULL;
 	while ((nread = read(fd1, buff, 1024)) > 0)
