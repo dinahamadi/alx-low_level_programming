6ae9f8fcf06250d008f1e0ac495c07898139e513
dina <dinahamadi.20@gmail.com>
2024-02-09 00:33:23 +0100

    binary

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - check the code
+ * @n: an unsigned number
+ * @index: the index
+ * Return: the value of the bit at index index or -1 if an error occured.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int result;
+
+	result = (n >> index) & 1;
+	if (result != 0 && result != 1)
+		return (-1);
+	return (result);
+}
