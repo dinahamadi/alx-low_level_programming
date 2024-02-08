fdc6011b5c2e83d9eff978bc103440594ed4de0e
dina <dinahamadi.20@gmail.com>
2024-02-09 00:37:06 +0100

    binary

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness - check the code
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+
+	if ((i >> 1) & 0)
+		return (0);
+	return (1);
+}
